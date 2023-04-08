5d0d220967ebca9d66ecd76eff4129307fbd28ca
shauraya <s_salwan164090@fanshaweonline.ca>
2023-04-07 20:50:06 -0400

    Made some additional changes in js

--- js/main.js
+++ js/main.js
@@ -14,4 +14,27 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+function func() {
+    var txt = document.getElementById("div2").innerText;
+    arr = txt.split(" ");
+    for (i = 0; i < arr.length; i++) {
+        snd1 = new Audio();
+        var src1 = document.createElement("source");
+        src1.type = "audio/mp3";
+        src1.src = "music/" + arr[i] + ".mp3";
+        snd1.appendChild(src1);
+        snd1.play();
+    }
+
+}
+
+function pauseaudio() {
+    for (i = 0; i < arr.length; i++) {
+        snd1.pause();
+    }
+
+}
+
+function reset() {
+    location.reload();
 }
